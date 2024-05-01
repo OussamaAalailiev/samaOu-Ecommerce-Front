51b1d712e95e6242dbc991b0628dba2cf7997409
OussamaAalailiev <aalaila.oussama@gmail.com>
2024-05-01 23:12:57 +0100

    modify categories cards + add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HeroSectionComponent],
+  imports: [RouterOutlet, NavComponent, HeroSectionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+})
+export class FooterComponent {
+  currentDate: Date = new Date();
+  storeName: string = 'SamaOu Ecommerce';
+}
