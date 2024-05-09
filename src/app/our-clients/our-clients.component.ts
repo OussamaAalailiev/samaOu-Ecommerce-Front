3ff6b4b6b4b2805e14bfe81c31c34989fa6b3ba1
OussamaAalailiev <aalaila.oussama@gmail.com>
2024-05-09 16:55:05 +0100

    categories less prices added + primeng carousel

--- src/app/our-clients/our-clients.component.ts
+++ src/app/our-clients/our-clients.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FaqComponent } from '../faq/faq.component';
+import { CategoriesLessPricesComponent } from '../categories-less-prices/categories-less-prices.component';
 
 @Component({
   selector: 'app-our-clients',
   standalone: true,
-  imports: [FaqComponent],
+  imports: [FaqComponent, CategoriesLessPricesComponent],
   templateUrl: './our-clients.component.html',
   styleUrl: './our-clients.component.css',
 })
